e691af4b5bfdbac0ef7d87ed5151dd1cbaf3b14d
alex-web-coder <alex-web-coder@proton.me>
2026-06-17 13:09:45 +0200

    addind notes dynamically

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Header } from "./header/header";
 import { Footer } from "./footer/footer";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Note } from './models/note.model';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer],
+  imports: [Header, Footer, CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
@@ -26,6 +29,42 @@ export class App {
 
   showAlert(): void {
     alert('H1 title clicked!');
+  }
+
+  protected readonly notes = signal<Note[]> ([
+    {
+      id: 1,
+      title: 'First Note',
+      content: 'This is the content of the first note.',
+      createdAt: new Date('2024-01-01T10:00:00')
+    },
+    {
+      id: 2,
+      title: 'Second Note',
+      content: 'This is the content of the second note.',
+      createdAt: new Date('2024-02-01T11:00:00')
+    }
+  ]);
+
+  protected newNote = signal<Partial<Note>>({
+    title: '',
+    content: ''
+  });
+
+  protected addNote(): void {
+    const newNoteValue = this.newNote();
+    if (newNoteValue.title && newNoteValue.content) {
+      const newNote: Note = {
+        id: this.notes().length + 1,
+        title: newNoteValue.title,
+        content: newNoteValue.content,
+        createdAt: new Date()
+      };
+      this.notes.update(notes => [...notes, newNote]);
+      this.newNote.set({ title: '', content: '' });
+    } else {
+      alert('Please enter both title and content for the note.');
+    }
   }
 
 }
