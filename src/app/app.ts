3c3272d876f301122624cc6e0dfb7a7d2c90d81e
alex-web-coder <alex-web-coder@proton.me>
2026-06-18 11:10:26 +0200

    about and home page added and some stylesheets

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,27 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component} from '@angular/core';
 import { Header } from "./header/header";
 import { Footer } from "./footer/footer";
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Note } from './models/note.model';
-import { NoteService } from './services/note';
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, CommonModule, FormsModule],
+  imports: [Header, Footer, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'Helloworld';
-  protected isMouseOver = false;
-
-  constructor(private noteService: NoteService) {}
-
-  protected changeTitle(event: MouseEvent): void {
-    this.title = 'Mouse click coordinates were: ' + event.clientX + ', ' + event.clientY;
-  }
-
   public getBookName(): string {
     return 'The Epic of Gilgamesh';
   }
@@ -29,28 +18,4 @@ export class App {
   protected handleFooterClick(event: MouseEvent): void {
     alert('Footer clicked! Mouse click coordinates were: ' + event.clientX + ', ' + event.clientY);
   }
-
-  showAlert(): void {
-    alert('H1 title clicked!');
-  }
-
-  protected newNote = signal<Partial<Note>>({
-    title: '',
-    content: ''
-  });
-
-  protected addNote(): void {
-    const newNoteValue = this.newNote();
-    if (!newNoteValue.title || !newNoteValue.content) {
-      alert('Please enter both title and content for the note.');
-    } else {
-      this.noteService.addNote(newNoteValue.title, newNoteValue.content);
-      this.newNote.set({ title: '', content: '' });
-    }
-  }
-
-  protected get notes() {
-    return this.noteService.getNotes();
-  }
-
 }
---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,49 @@
+import { Component, signal } from '@angular/core';
+import { Note } from '../../models/note.model';
+import { NoteService } from '../../services/note';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+
+  constructor(private noteService: NoteService) {}
+
+  protected title = 'Helloworld';
+  protected isMouseOver = false;
+
+  protected changeTitle(event: MouseEvent): void {
+    this.title = 'Mouse click coordinates were: ' + event.clientX + ', ' + event.clientY;
+  }
+
+
+
+  
+  showAlert(): void {
+    alert('H1 title clicked!');
+  }
+
+  protected newNote = signal<Partial<Note>>({
+    title: '',
+    content: ''
+  });
+
+  protected addNote(): void {
+    const newNoteValue = this.newNote();
+    if (!newNoteValue.title || !newNoteValue.content) {
+      alert('Please enter both title and content for the note.');
+    } else {
+      this.noteService.addNote(newNoteValue.title, newNoteValue.content);
+      this.newNote.set({ title: '', content: '' });
+    }
+  }
+
+  protected get notes() {
+    return this.noteService.getNotes();
+  }
+}
